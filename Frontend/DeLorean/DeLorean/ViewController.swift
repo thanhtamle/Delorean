a9b5342623d9c7cb4b2455f3d2cea425f73675ac
thanhtamle <thanhtam.le@citynow.jp>
2016-09-16 15:09:49 +0700

    Init frontend(iOS) delorean project

---
+++ Frontend/DeLorean/DeLorean/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeLorean
+//
+//  Created by Thanh-Tam Le on 9/16/16.
+//  Copyright © 2016 Thanh-Tam Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
